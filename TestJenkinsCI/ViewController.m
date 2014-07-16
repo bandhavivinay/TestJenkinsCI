1b787868f99481c50da58b68a4af497c1b4ce97a
bandhavivinay <tavant@BANDHAVIVINAY-MAC.local>
2014-07-16 17:40:36 +0530

    new rule exception

--- TestJenkinsCI/ViewController.m
+++ TestJenkinsCI/ViewController.m
@@ -24,6 +24,47 @@
     if(true){
        
     }
+    else{
+        
+    }
+    
+    int var = 1;
+    abc:
+    switch (var) {
+        case 1:
+            NSLog(@"Do Nothing");
+            break;
+            
+        default:
+            break;
+    }
+    
+    goto abc;
+    
+    for(;var>1;){
+        NSLog(@"Do Nothing");
+    }
+    
+    for(int i=0;i<2;i++){
+        
+    }
+    
+    while(1){
+        
+    }
+    
+    NSString* test = @"TEST";
+    
+    @try {
+        
+        
+    }
+    @catch (NSException * e) {
+        NSLog(@"Exception: %@", e);
+    }
+    @finally {
+        
+    }
     
     //test comment ...
 	// Do any additional setup after loading the view, typically from a nib.
